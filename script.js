d0f606b5bf79e2859e59b2b3047d818f5998c6e3
Daniel Felipe <daniel.companyservicostecnicos@gmail.com>
2022-07-18 14:35:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var iframe = document.createElement('iframe');
+iframe.height = 600;
+iframe.width = 600;
+iframe.src = 'https://dos.zone/player/?bundleUrl=https%3A%2F%2Fcdn.dos.zone%2Fcustom%2Fdos%2Fdoom.jsdos?anonymous=1'
+document.body.appendChild(iframe);
